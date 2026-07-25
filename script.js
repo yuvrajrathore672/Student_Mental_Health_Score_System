93f87a500dcb7f0b5ba1ed997e4dd97fb676a607
Yuvraj Rathore <yuvrajrathore948@gmail.com>
2026-07-25 16:34:17 +0530

    Final touch

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const API_BASE = "https://mansik-santulan-score.onrender.com";
+  const API_BASE = "https://student-mental-health-score-system.onrender.com";
 
   const form = document.getElementById("predict-form");
   const submitBtn = document.getElementById("submit-btn");

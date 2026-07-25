d67809847c61f7c04cc237af736c0713afda188a
yuvrajrathore672 <yuvrajrathore948@gmail.com>
2026-07-25 16:09:09 +0530

    Updated JS file with deployed backend link

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000/predict";
+const API_URL = "https://mental-health-score-qk3v.onrender.com";
 
 const form = document.getElementById("predictForm");
 const submitBtn = document.getElementById("submitBtn");
